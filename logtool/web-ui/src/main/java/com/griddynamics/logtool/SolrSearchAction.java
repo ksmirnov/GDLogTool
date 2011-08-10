1fcfbf1af1faf9c13280ab940699a1763ed764c0
vpolyakov <vpolyakov@vpolyakov.ws>
2011-08-10 14:07:35 +0400

    some debug stuff removed

--- logtool/web-ui/src/main/java/com/griddynamics/logtool/SolrSearchAction.java
+++ logtool/web-ui/src/main/java/com/griddynamics/logtool/SolrSearchAction.java
@@ -5,8 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,58 +23,16 @@ public class SolrSearchAction extends Action {
     }
 
     public void doGrepOverSolr(String query, String request, int pageSize, ServletOutputStream sos) {
-            List<Map<String, String>> lol = new ArrayList<Map<String, String>>();
-            Map<String, String> sad = new HashMap<String, String>();
-            sad.put("path", "/home/vpolyakov/workspace/GDLogTool/logs/testApp/localhost/testInstance/2011-15-Jul.log");
-            sad.put("startIndex", "10");
-            sad.put("length", "1100");
-            sad.put("application", "testApp");
-            sad.put("host", "localhost");
-            sad.put("instance", "testInstance");
-            sad.put("date", "2011-15-Jul");
-            lol.add(sad);
-            sad = new HashMap<String, String>();
-            sad.put("path", "/home/vpolyakov/workspace/GDLogTool/logs/testApp/localhost/testInstance1/2011-15-Jul.log");
-            sad.put("startIndex", "10");
-            sad.put("length", "480");
-            sad.put("application", "testApp");
-            sad.put("host", "localhost");
-            sad.put("instance", "testInstance1");
-            sad.put("date", "2011-15-Jul");
-            lol.add(sad);
-
         try {
-            sos.print(storage.doGrepOverSolrSearch(lol, request, pageSize).toString());
-            //sos.print(storage.doGrepOverSolrSearch(searchServer.search(query), request, pageSize).toString());
+            sos.print(storage.doGrepOverSolrSearch(searchServer.search(query), request, pageSize).toString());
         } catch (IOException ex) {
 
         }
     }
 
     public void doSolrSearch(String query, ServletOutputStream sos) {
-            List<Map<String, String>> lol = new ArrayList<Map<String, String>>();
-            Map<String, String> sad = new HashMap<String, String>();
-            sad.put("path", "/home/vpolyakov/workspace/GDLogTool/logs/testApp/localhost/testInstance/2011-15-Jul.log");
-            sad.put("startIndex", "10");
-            sad.put("length", "1100");
-            sad.put("application", "testApp");
-            sad.put("host", "localhost");
-            sad.put("instance", "testInstance");
-            sad.put("date", "2011-15-Jul");
-            lol.add(sad);
-            sad = new HashMap<String, String>();
-            sad.put("path", "/home/vpolyakov/workspace/GDLogTool/logs/testApp/localhost/testInstance1/2011-15-Jul.log");
-            sad.put("startIndex", "10");
-            sad.put("length", "480");
-            sad.put("application", "testApp");
-            sad.put("host", "localhost");
-            sad.put("instance", "testInstance1");
-            sad.put("date", "2011-15-Jul");
-            lol.add(sad);
-
         try {
-            sos.print(getJsonFromListMap(lol));
-            //sos.print(getJsonFromListMap(searchServer.search(query)));
+            sos.print(getJsonFromListMap(searchServer.search(query)));
         } catch (IOException ex) {
 
         }
