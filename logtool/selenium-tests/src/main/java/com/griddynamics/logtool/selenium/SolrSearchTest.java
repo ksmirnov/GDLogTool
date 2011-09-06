4c6da58b89f3288b5b5b22d9a553f9a515f59f8a
Livotov Stanislav <slivotov@griddynamics.com>
2011-09-06 17:01:48 +0400

    SolrSearchTest for Selenium

---
+++ logtool/selenium-tests/src/main/java/com/griddynamics/logtool/selenium/SolrSearchTest.java
@@ -0,0 +1,48 @@
+package com.griddynamics.logtool.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SolrSearchTest extends SeleniumTest {
+
+    public SolrSearchTest(String uiHost, int uiPort, WebDriver webDriver) {
+        super(uiHost, uiPort, webDriver);
+    }
+
+    public boolean perform(String tcpHost, int tcpPort) {
+        try {
+            Utils.tcpSend(tcpHost, tcpPort, "Selenium fast message", "SeleniumTestApp.SeleniumInst", 10, 100);
+            Thread.sleep(100);
+            driver.get(uiHost + ":" + uiPort);
+            driver.findElement(By.xpath("//span[contains(text(), 'Search')]")).click();
+            driver.findElement(By.xpath("//input[@role='textbox']")).sendKeys("fast");
+            driver.findElement(By.xpath("//div[@class='x-toolbar x-window-item x-toolbar-footer " +
+                    "x-docked x-docked-bottom x-toolbar-docked-bottom x-toolbar-footer-docked-bottom " +
+                    "x-box-layout-ct']//span[contains(text(), 'Search')]")).click();
+            waitForElementIsPresent(By.xpath("//div[@id='searchResGrid']//table//tr[1]"), 50);
+            int tr = 2;
+            while(true){
+                try{
+                    driver.findElement(By.xpath("//div[@id='searchResGrid']//table//tr[" + tr + "]"));
+                    tr++;
+                } catch (NoSuchElementException e){
+                    break;
+                }
+            }
+            Utils.deleteDirectory(uiHost, uiPort, "SeleniumTestApp");
+            tearDown();
+            if(tr == 12) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
