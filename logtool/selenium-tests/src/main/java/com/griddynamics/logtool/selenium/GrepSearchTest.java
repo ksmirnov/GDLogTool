366436d575ba3bd2c516f209a7b6e402cf11cd10
Konstantin Smirnov <ksmirnov@griddynamics.com>
2011-09-08 11:31:57 +0400

    throws Exception moved to method header for grep & solr search tests

--- logtool/selenium-tests/src/main/java/com/griddynamics/logtool/selenium/GrepSearchTest.java
+++ logtool/selenium-tests/src/main/java/com/griddynamics/logtool/selenium/GrepSearchTest.java
@@ -12,30 +12,25 @@ public class GrepSearchTest extends SeleniumTest {
         super(uiHost, uiPort, tcpHost, tcpPort, webDriver);
     }
 
-    public boolean perform() {
-        try {
-            Utils.tcpSend(tcpHost, tcpPort, "Selenium fast message", "SeleniumTestApp.SeleniumInst", 10, 100);
-            Thread.sleep(100);
-            driver.get(uiHost + ":" + uiPort);
-            clickLogAfterInstance("SeleniumInst");
-            driver.findElement(By.xpath("//span[contains(text(), 'Search')]")).click();
-            driver.findElement(By.xpath("//input[@role='textbox']")).sendKeys("grep: fast");
-            driver.findElement(By.xpath("//div[@class='x-toolbar x-window-item x-toolbar-footer " +
-                    "x-docked x-docked-bottom x-toolbar-docked-bottom x-toolbar-footer-docked-bottom " +
-                    "x-box-layout-ct']//span[contains(text(), 'Search')]")).click();
-            waitForElementIsPresent(By.xpath("//div[@id='searchResGrid']//table//tr[2]//td[2]//div"), 50);
-            Selenium selenium = new WebDriverBackedSelenium(driver, uiHost + ":" + uiPort);
-            int count = Integer.parseInt(selenium.getText("xpath=//div[@id='searchResGrid']//table//tr[2]//td[2]//div"));
-            Utils.deleteDirectory(uiHost, uiPort, "SeleniumTestApp");
-            if(count == 10) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    public boolean perform() throws Exception {
+        Utils.tcpSend(tcpHost, tcpPort, "Selenium fast message", "SeleniumTestApp.SeleniumInst", 10, 100);
+        Thread.sleep(100);
+        driver.get(uiHost + ":" + uiPort);
+        clickLogAfterInstance("SeleniumInst");
+        driver.findElement(By.xpath("//span[contains(text(), 'Search')]")).click();
+        driver.findElement(By.xpath("//input[@role='textbox']")).sendKeys("grep: fast");
+        driver.findElement(By.xpath("//div[@class='x-toolbar x-window-item x-toolbar-footer " +
+                "x-docked x-docked-bottom x-toolbar-docked-bottom x-toolbar-footer-docked-bottom " +
+                "x-box-layout-ct']//span[contains(text(), 'Search')]")).click();
+        waitForElementIsPresent(By.xpath("//div[@id='searchResGrid']//table//tr[2]//td[2]//div"), 50);
+        Selenium selenium = new WebDriverBackedSelenium(driver, uiHost + ":" + uiPort);
+        int count = Integer.parseInt(selenium.getText("xpath=//div[@id='searchResGrid']//table//tr[2]//td[2]//div"));
+        Utils.deleteDirectory(uiHost, uiPort, "SeleniumTestApp");
+        if(count == 10) {
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
 }
